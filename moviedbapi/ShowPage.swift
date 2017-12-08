2748874334237b9480a3705205ce6d91cf2c14af
tmtrademark90 <31859785+tmtrademark90@users.noreply.github.com>
2017-12-08 15:11:40 -0600

    2nd upload.
    working on view controllers. Brought in alamofire and swiftyjson

---
+++ moviedbapi/ShowPage.swift
@@ -0,0 +1,35 @@
+//
+//  ShowPage.swift
+//  moviedbapi
+//
+//  Created by Terry McCart on 12/8/17.
+//  Copyright © 2017 Terry McCart. All rights reserved.
+//
+
+import UIKit
+
+class ShowPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
